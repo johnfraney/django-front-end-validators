8fbbce4daf73195c6744dd6b0549e4c8fb0dccef
John Franey <johnfraney@gmail.com>
2018-10-11 00:11:38 -0300

    Fix JS bug with multiple validators

--- front_end_validators/static/js/front_end_validators.js
+++ front_end_validators/static/js/front_end_validators.js
@@ -2,17 +2,18 @@ var fieldsToValidate = document.querySelectorAll('[data-validators]')
 for (fieldToValidate of fieldsToValidate) {
   fieldToValidate.oninput = function() {
     var fieldValidators = eval(this.attributes['data-validators'].value)
+    var failingValidators = []
     for (validatorFunction of fieldValidators) {
       try {
         validatorFunction(this.value)
-        var fieldIsValid = this.checkValidity()
-        if (fieldIsValid) {
-          this.setCustomValidity('')
-        }
       }
       catch(e) {
         this.setCustomValidity(e.message)
+        failingValidators.push(validatorFunction)
       }
+    }
+    if (failingValidators.length === 0) {
+      this.setCustomValidity('')
     }
   }
 }
